ec9eabfd0bee0e1b9f4e7dcfe448ffde4eec4caf
Izardo <44006825+Izardo@users.noreply.github.com>
2022-01-06 15:58:08 +0000

    Delete dbconfig.py

--- dbconfig.py
+++
@@ -1,6 +0,0 @@
-mysql = {
-    'host' : 'izzymcizzface.mysql.pythonanywhere-services.com',
-    'user' : 'izzymcizzface',
-    'password' : 'wendy567',
-    'database' : 'izzymcizzface$native_irish_trees', 
-}
